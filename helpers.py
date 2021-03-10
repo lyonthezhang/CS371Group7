747f0805c439fe8c5a252569d0b27117e6a933b9
Megs  Yadav <meghnayadav777@gmail.com>
2021-03-10 04:47:10 -0600

    New Query and Get Final Answer helpers

--- helpers.py
+++ helpers.py
@@ -10,7 +10,7 @@ from wikidata.client import Client
 
 # Get the id of an entity from the url
 def extract_id_from_url(url_string):
-	return url_string.split('/')[-1]
+    return url_string.split('/')[-1]
 
 
 # Check if the entity id is one of the entites pre-selected to be relevant
@@ -20,19 +20,19 @@ def valid_id(entity_id):
 
 # Given a list of ids. Get the time zone given an entity id. This is because wikidata does not have a standard for storing the time_zone for places
 def get_time_zone(entity_id_lst):
-    
+
     for ent_id in entity_id_lst:
         for zone in TIME_ZONES:
             if ent_id in zone:
                 return zone[0]
-    
+
     return entity_id_lst[0]
 
 
 # Helper for get_state. Param is a list of entities. It goes through and check if each entity is an istance of a us_state or a us_territory.
 # If it is then you know that it is a state
 def get_state_helper(potential_states):
-    
+
     for p_state in potential_states:
         # ************************************** note ******************************************************
         # doing .lists() sometimes fails for some reason (it is because of an error in the wikidata library)
@@ -45,7 +45,7 @@ def get_state_helper(potential_states):
                 for value in values:
                     if get_identifier(value) == US_STATE or get_identifier(value) == US_TERRITORY:
                         return get_identifier(p_state)
-    
+
     return None
 
 
@@ -53,7 +53,7 @@ def get_state_helper(potential_states):
 def get_state(potential_states):
 
     state_id = get_state_helper(potential_states)
-    
+
     if state_id:
         return state_id
 
@@ -73,7 +73,7 @@ def get_state(potential_states):
         if get_identifier(attribute) == LOCATED_IN_TERRITORY:
             for value in values:
                 potential_states2.append(value)
-    
+
     state_id = get_state_helper(potential_states2)
 
     return state_id
@@ -99,6 +99,8 @@ def get_identifier(dat):
         return "UNKNOWN"
 
 # Maps a continous numerical population to a category
+
+
 def get_population(num):
     if num < 1000:
         return LESS_THAN_1000
@@ -114,6 +116,8 @@ def get_population(num):
         return MORE_THAN_1_MIL
 
 # Maps a continous numerical inception year to a category
+
+
 def get_inception(num):
     if num < 1600:
         return BEFORE_1600S
@@ -129,6 +133,8 @@ def get_inception(num):
         return DURING_2000S
 
 # Maps a continous numerical area to a category
+
+
 def get_area(num):
     if num < 100:
         return UNDER_100
@@ -141,16 +147,18 @@ def get_area(num):
     else:
         return OVER_1500
 
+
 def print_matrix(matrix):
-    f = open("matrix.txt","w")
+    f = open("matrix.txt", "w")
     for line in matrix:
         for item in line:
-            f.write(str(item) + " "*(25-len(str(item))) + "|" )
+            f.write(str(item) + " "*(25-len(str(item))) + "|")
         f.write("\n")
     f.close()
 
+
 def print_df(df):
-    f = open("matrix_one_hot.txt","w")
+    f = open("matrix_one_hot.txt", "w")
     df.to_string(f)
     f.close()
 
@@ -176,7 +184,7 @@ def format_question(selection):
             return "Does the city have between 500,000 and 1,000,000 residents?"
         elif value == MORE_THAN_1_MIL:
             return "Does the city have more than 1 million residents?"
-    
+
     # ask if a city has a specific area
     elif attribute == AREA:
         if value == UNDER_100:
@@ -189,7 +197,7 @@ def format_question(selection):
             return "Does the city have an area of between 1000 and 1500 square kilometers?"
         elif value == OVER_1500:
             return "Does the city have an area of over 1500 square kilometers?"
-    
+
     # ask if a city was founded during a specific time span
     elif attribute == INCEPTION_DATE:
         if value == BEFORE_1600S:
@@ -204,7 +212,7 @@ def format_question(selection):
             return "Was the city founded during the 1900s?"
         if value == DURING_2000S:
             return "Was the city founded during the 2000s?"
-    
+
     # ask if a city is located in a specific time zone
     elif attribute == LOCATED_IN_TIME_ZONE:
         if value == TIME_ZONES[0][0]:
@@ -221,21 +229,22 @@ def format_question(selection):
             return "Does the city reside in Hawaiian time zone?"
         else:
             return f"Does the city's time zone correspond with the {get_name(value)} time zone?"
-            
+
     # ask question about whether city is located in specific territory
     elif attribute == LOCATED_IN_TERRITORY:
         return f"Is the city located in {get_name(value)}?"
-    
+
     # ask question about whether city is an instance of a specific thing
-    elif attribute == INSTANCE_OF: 
+    elif attribute == INSTANCE_OF:
         return f"Is this city a {get_name(value)}?"
 
 
-
 # Initialize wikidata client once
-wiki_data_client  = Client()
+wiki_data_client = Client()
 
 # return the name corresponding to the entity ID
+
+
 def get_name(entity_id):
     entity = wiki_data_client.get(entity_id, load=True)
     if type(entity) == Entity:
@@ -246,10 +255,122 @@ def get_name(entity_id):
 # on to the old_query that can be run on SPARQL
 # See column names in "matrix_one_hot.txt" to see what the selections are going to look like
 # Note: the column names in "matrix_one_hot.txt" are not an all inclusive list on the possible selections
-def construct_new_query(old_query, selection, answer):
-    
-    raise NotImplementedError
 
+
+def construct_new_query(old_query, selection, answer):
+    attribute = selection.split("_")[0]
+    value = "_".join(selection.split("_")[1:])
+
+    if attribute == POPULATION:
+        if value == MORE_THAN_1_MIL:
+            if answer == "y":
+                newQuery = str(
+                    "?item  wdt:P1082 ?pop . FILTER ( ?pop >  1000000 ).")
+            else:
+                newQuery = str(
+                    "?item  wdt:P1082 ?pop . FILTER ( ?pop <=  1000000 ).")
+        else:
+            if value == LESS_THAN_1000:
+                lb = 0
+                ub = 1000
+            elif value == BETWEEN_1000_AND_50000:
+                lb = 1000
+                ub = 50000
+            elif value == BETWEEN_50000_AND_100000:
+                lb = 50000
+                ub = 100000
+            elif value == BETWEEN_100000_AND_500000:
+                lb = 100000
+                ub = 500000
+            elif value == BETWEEN_500000_AND_1MIL:
+                lb = 500000
+                ub = 1000000
+            if answer == "y":
+                newQuery = str(
+                    "?item  wdt:P1082 ?pop . FILTER ( ?pop > " + str(lb) + " && ?pop <= " + str(ub) + ").")
+            else:
+                newQuery = str(
+                    "?item  wdt:P1082 ?pop . FILTER ( ?pop <= " + str(lb) + " || ?pop > " + str(ub) + ").")
+
+    elif attribute == AREA:
+        if value == OVER_1500:
+            if answer == "y":
+                newQuery = str(
+                    "?item  wdt:P1082 ?area . FILTER ( ?area > 1500).")
+            else:
+                newQuery = str(
+                    "?item  wdt:P1082 ?area . FILTER ( ?area <= 1500).")
+
+        else:
+            if value == UNDER_100:
+                lb = 0
+                ub = 100
+            elif value == BETWEEN_100_AND_500:
+                lb = 100
+                ub = 500
+            elif value == BETWEEN_500_AND_1000:
+                lb = 500
+                ub = 1000
+            elif value == BETWEEN_1000_AND_1500:
+                lb = 1000
+                ub = 1500
+            if answer == "y":
+                newQuery = str(
+                    "?item  wdt:P2046 ?area . FILTER ( ?area > " + str(lb) + " && ?area <= " + str(ub) + ").")
+            else:
+                newQuery = str(
+                    "?item  wdt:P2046 ?area . FILTER ( ?area <= " + str(lb) + " || ?area > " + str(ub) + ").")
+
+    elif attribute == INCEPTION_DATE:
+        if value == BEFORE_1600S:
+            if answer == "y":
+                newQuery = str(
+                    "?item wdt:P571 ?date . FILTER(1600 > YEAR(?date)  ) .")
+            else:
+                newQuery = str(
+                    "?item wdt:P571 ?date . FILTER(1600 < YEAR(?date)  ) .")
+        else:
+            if value == DURING_1600S:
+                lb = 1600
+                ub = 1700
+            elif value == DURING_1700S:
+                lb = 1700
+                ub = 1800
+            elif value == DURING_1800S:
+                lb = 1800
+                ub = 1900
+            elif value == DURING_1900S:
+                lb = 1900
+                ub = 2000
+            elif value == DURING_2000S:
+                lb = 2000
+                ub = 3000
+            if answer == "y":
+                newQuery = str("?item wdt:P571 ?date . FILTER(" + str(lb) +
+                               " <= YEAR(?date) && YEAR(?date) < " + str(ub) + " ) .")
+            else:
+                newQuery = str("?item wdt:P571 ?date . FILTER(" + str(lb) +
+                               " > YEAR(?date) || YEAR(?date) > " + str(ub) + " ) .")
+    elif attribute == LOCATED_IN_TERRITORY:
+        if answer == "y":
+            newQuery = str("{?item  wdt:P131 ?county . ?county wdt:P131 wd:" +
+                           value + " .} UNION {?item wdt:P131 wd:" + value + " .}")
+        else:
+            newQuery = str("{?item  wdt:P131 ?county} MINUS {?county wdt:P131 wd:" +
+                           value + " } FILTER NOT EXISTS{(?item wdt:P131 wd:" + value + ")}")
+
+    else:
+        newQuery = str("?item wdt:" + attribute + " wd:" + value)
+
+        if answer == "n":
+            newQuery = str("FILTER NOT EXISTS{(" + newQuery + ")}")
+        else:
+            newQuery = str(newQuery + ".")
+
+    oldQuery1 = old_query.split("SERVICE", 1)[0]
+    oldQuery2 = old_query.split("SERVICE", 1)[1]
+    newQuery = str(oldQuery1 + " " + newQuery + " " "SERVICE" + oldQuery2)
+    return newQuery
 
 # Take a really long query that has narrowed down the options to some degree.
 # Can implement this one of two ways:
@@ -257,6 +378,8 @@ def construct_new_query(old_query, selection, answer):
 #   2. Run the query. Get the first 10 results or so. And return them as potential answers
 # Second option is easier IMO
 # Make sure you retun a string either way
-def get_final_answer(query):
-    
-    raise NotImplementedError
+
+
+def get_final_answer(results):
+
+    return str(results[1:10])
--- questionsbot.py
+++ questionsbot.py
@@ -1,4 +1,4 @@
-### Imports up here
+# Imports up here
 from SPARQLWrapper import SPARQLWrapper, JSON
 from wikidata.client import Client
 from helpers import *
@@ -9,13 +9,13 @@ from sklearn.preprocessing import OneHotEncoder
 
 
 # From https://www.wikidata.org/wiki/Wikidata:SPARQL_query_service/queries/examples#Cats
-### Querying Wikidata
+# Querying Wikidata
 
 # Run a sparql query
 def run_sparql_query(query):
     sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
     sparql.setQuery(query)
-    
+
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
 
@@ -23,9 +23,11 @@ def run_sparql_query(query):
 
 
 # Initialize wikidata client once
-wiki_data_client  = Client()
+wiki_data_client = Client()
 
 # Get attributes of a data using wikidata library
+
+
 def get_attributes(data):
 
     url = data['item']['value']
@@ -42,16 +44,16 @@ def get_attributes(data):
 # Keeping this commented in case we need to reference other functions for entities
 # print(entity.__dir__())
 
-###### THE END RESULT
-###### info contains P:Q mappings (instance of: big city for example for New York)
-###### info is a list, where each entry is a tuple of size 2 containing a 1) value containing the P and 2) another list containing relevant entities  
+# THE END RESULT
+# info contains P:Q mappings (instance of: big city for example for New York)
+# info is a list, where each entry is a tuple of size 2 containing a 1) value containing the P and 2) another list containing relevant entities
 
 
 # Constructs a matrix (list of lists) given a list of data_points
 def construct_matrix(cities_data):
     columns = ["city"]
     columns_set = set(columns)
-    column_location = {"city" : 0}
+    column_location = {"city": 0}
 
     # This for loop goes through all of the cities_data and constructs all of the columns
     for city_data in cities_data:
@@ -63,7 +65,7 @@ def construct_matrix(cities_data):
 
             if not(valid_id(a_id)):
                 continue
-        
+
             if a_id == INSTANCE_OF:
                 for value in values:
                     col_name = a_id + "_" + get_identifier(value)
@@ -79,15 +81,14 @@ def construct_matrix(cities_data):
                     column_location[col_name] = len(columns)
                     columns.append(col_name)
                     columns_set.add(col_name)
-                
-    
+
     table = [columns]
 
     # Go through all of the cities_data and fill out each city's data for each column
     for city_data in cities_data:
 
         city_id, attribute_data = city_data[0], city_data[1]
-        
+
         city_info = [0]*len(columns)
         city_info[0] = city_id
 
@@ -97,7 +98,7 @@ def construct_matrix(cities_data):
 
             if not(valid_id(a_id)):
                 continue
-            
+
             if a_id == INSTANCE_OF:
                 for value in values:
                     col_name = a_id + "_" + get_identifier(value)
@@ -109,19 +110,21 @@ def construct_matrix(cities_data):
             elif a_id == INCEPTION_DATE:
                 index = column_location[a_id]
                 col_val = get_inception(get_identifier(values[0]))
-            
+
             elif a_id == AREA:
                 index = column_location[a_id]
                 col_val = get_area(get_identifier(values[0]))
-            
+
             elif a_id == POPULATION:
                 index = column_location[a_id]
-                col_val = get_population(max([get_identifier(population) for population in values]))
+                col_val = get_population(
+                    max([get_identifier(population) for population in values]))
 
             elif a_id == LOCATED_IN_TIME_ZONE:
                 index = column_location[a_id]
-                col_val = get_time_zone([get_identifier(val) for val in values])
-            
+                col_val = get_time_zone([get_identifier(val)
+                                         for val in values])
+
             elif a_id == LOCATED_IN_TERRITORY:
                 index = column_location[a_id]
                 col_val = get_state(values)
@@ -129,16 +132,18 @@ def construct_matrix(cities_data):
             city_info[index] = col_val
 
         table.append(city_info)
-    
+
     return table
 
 # Returns a one hot encoded pandas df given a matrix(list of lists)
+
+
 def one_hot_encode(matrix):
-    
+
     # Make pandas df
-    df = pd.DataFrame(matrix[1:], columns = matrix[0])
+    df = pd.DataFrame(matrix[1:], columns=matrix[0])
     encoder = OneHotEncoder()
-    
+
     # Some cities don't have timezone (wikidata is whack)
     for i in range(df.shape[0]):
         time_zone_value = df.loc[i, LOCATED_IN_TIME_ZONE]
@@ -150,14 +155,22 @@ def one_hot_encode(matrix):
     new_column_names = encoder.get_feature_names(CATEGORICAL_IDS)
 
     # Add one hot encoded data to original df
-    one_hot_encoded_df = df.join(pd.DataFrame(oe_results.toarray(), columns=new_column_names))
+    one_hot_encoded_df = df.join(pd.DataFrame(
+        oe_results.toarray(), columns=new_column_names))
 
     # Drop the orginial catergorical columns (repalced with one hot encoded columns) and the NA timezone one
-    one_hot_encoded_df = one_hot_encoded_df.drop(columns = CATEGORICAL_IDS+[LOCATED_IN_TIME_ZONE+"_NA"])
+    if "P421_NA" in one_hot_encoded_df.columns:
+        one_hot_encoded_df = one_hot_encoded_df.drop(
+            columns=CATEGORICAL_IDS+[LOCATED_IN_TIME_ZONE+"_NA"])
+    else:
+        one_hot_encoded_df = one_hot_encoded_df.drop(
+            columns=CATEGORICAL_IDS)
 
     return one_hot_encoded_df
 
 # Give a one hot encoded pandas df, return a column name to split on
+
+
 def select_attribute(df):
     best_diff = float("inf")
     best_col = ""
@@ -169,9 +182,9 @@ def select_attribute(df):
         if diff < best_diff:
             best_col = col_name
             best_diff = diff
-    
+
     return best_col
-        
+
 
 # The following has the general workflow
 # It takes a query, it finds an attribute to split on, and it returns it
@@ -193,24 +206,28 @@ def find_attribute_to_split_on(query):
     # Construct a matrix
     matrix = construct_matrix(cities_data)
 
-    print_matrix(matrix) # Uncomment/Comment this if you want it to print the matrix in "matrix.txt"
-    
+    # Uncomment/Comment this if you want it to print the matrix in "matrix.txt"
+    print_matrix(matrix)
+
     # Get the one hot encoded pandas df of the matrix
     one_hot = one_hot_encode(matrix)
 
-    print_df(one_hot) # Uncomment/Comment this if you want it to print the matrix in "matrix_one_hot.txt"
+    # Uncomment/Comment this if you want it to print the matrix in "matrix_one_hot.txt"
+    print_df(one_hot)
 
     # Select the attribute to split on
-    selection  = select_attribute(one_hot)
+    selection = select_attribute(one_hot)
 
     # Return the selection
     return selection
 
 # This is the game. Run this to run the game
+
+
 def game():
-    
+
     print("Welcome to our 20 Questions Bot")
-    
+
     query = INITIAL_QUERY
 
     for question_number in range(20):
@@ -218,7 +235,7 @@ def game():
         selection = find_attribute_to_split_on(query)
         # Ask user a question based on the selection
         question = format_question(selection)
-        
+
         print(question)
         answer = input("(y/n): ")
 
@@ -226,6 +243,10 @@ def game():
         query = construct_new_query(query, selection, answer)
 
     # query is now a long query that has narrowed down the cities to some degree. Get some sort of final answer from that query
-    final_answer = get_final_answer(query)
+    final_results = run_sparql_query(query)
+    final_answer = get_final_answer(final_results)
 
     print(f"Is your answer {final_answer}")
+
+
+game()
